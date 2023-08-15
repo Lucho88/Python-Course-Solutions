7b0d9a8bf8bbaa0992cebd0c3801899a0a9fbaf3
Lucho88 <lucho1899@yahoo.com>
2023-08-15 15:44:11 +0300

    Fixed task_110 so it can work with long strings

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import task_101_initial
-import task_101_solution
-# import task_110_initial
+# import task_101_initial
+# import task_101_solution
+import task_110_initial
+import asyncio
+
+
+import task_115_solution
 
 
 # task_101_initial.run()
 # task_101_solution.run()
 # task_110_initial.run()
+# task_115_solution.run()
 
 
+if __name__ == '__main__':
+    finnhub_api_token = 'cjd12r9r01qnak8idrogcjd12r9r01qnak8idrp0'
+    btc_price_tracker = task_115_solution.RealTimeBitcoinPrice(finnhub_token=finnhub_api_token)
 
-
-
-
-
+    try:
+        asyncio.get_event_loop().run_until_complete(btc_price_tracker.connect())
+    except KeyboardInterrupt:
+        print("Script manually terminated.")
 
--- task_110_solution.py
+++ task_110_solution.py
@@ -7,14 +7,17 @@ printed_permutations = 0
 def generate_permutations(a, n):
     global printed_permutations
     if n == 0:
-        print(''.join(a))
+        sys.exit(0)
     else:
-        for i in range(n):
-            generate_permutations(a, n-1)
-            j = random.randint(0, i)
+        for i in range(n + 1):
+            j = random.randint(0, n)
+            print(j, '-j')
+            print(n, '-n')
             a[j], a[n] = a[n], a[j]
+            print(''.join(a))
+            generate_permutations(a, n - 1)
             printed_permutations += 1
-            if printed_permutations >= 20:
+            if printed_permutations >= 50:
                 sys.exit(0)
         generate_permutations(a, n-1)
 
@@ -23,7 +26,7 @@ if len(sys.argv) != 2:
     sys.stderr.write('Exactly one argument is required\n')
     sys.exit(1)
 
+
 word = sys.argv[1]
-
+print('the length of the word - 1 is', len(word) - 1)
 generate_permutations(list(word), len(word) - 1)
-
