3ada627dca79dd73bf798ceabc730baf3b7587a4
Lucho88 <lucho1899@yahoo.com>
2023-08-17 17:56:30 +0300

    Task 115 finished. One issue yet to be sorted - shows the same time interval when the data reffers to different ones

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 # import task_101_solution
 # import task_110_initial
 import websocket
-
-import task_115_solution
+from task_115_solution import DataProcess
 
 
 # task_101_initial.run()
@@ -13,11 +12,12 @@ import task_115_solution
 
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
+    data_process = DataProcess()
+    # websocket.enableTrace(True)
     ws = websocket.WebSocketApp("wss://ws.finnhub.io?token=cjd12r9r01qnak8idrogcjd12r9r01qnak8idrp0",
-                                on_message =task_115_solution.on_message,
-                                on_error =task_115_solution.on_error,
-                                on_close =task_115_solution.on_close)
-    ws.on_open = task_115_solution.on_open
+                                on_message=data_process.on_message,
+                                on_error=data_process.on_error,
+                                on_close=data_process.on_close)
+    ws.on_open = data_process.on_open
     ws.run_forever()
 
--- task_115_solution.py
+++ task_115_solution.py
@@ -1,42 +1,83 @@
 import json
 import datetime
+import time
 
 
-def process_live_data(json_data):
-    try:
-        parsed_data = json.loads(json_data)
-        if "data" in parsed_data and isinstance(parsed_data["data"], list):
-            for entry in parsed_data["data"]:
-                if "t" in entry and "p" in entry and "v" in entry:
-                    timestamp = entry["t"] / 1000
-                    formatted_timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-                    price = entry["p"]
-                    volume = entry["v"]
+class DataProcess:
+    def __init__(self):
+        self.raw_data = []  # Store raw data here
+        self.last_average_time = None
 
-                    print(f"{formatted_timestamp} price:{price} volume:{volume:.5f}")
-                else:
-                    print("Invalid data entry:", entry)
-        else:
-            print("Invalid JSON data format.")
+    def process_live_data(self, json_data):
+        try:
+            parsed_data = json.loads(json_data)
+            if "data" in parsed_data and isinstance(parsed_data["data"], list):
+                for entry in parsed_data["data"]:
+                    if "t" in entry and "p" in entry and "v" in entry:
+                        timestamp = entry["t"] / 1000
+                        formatted_timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+                        price = entry["p"]
+                        formatted_price = f"{price:.2f}"
+                        volume = entry["v"]
+                        self.raw_data.append({'timestamp': timestamp, 'price': price, 'volume': volume})
+                        formatted_line = "{} price:{:<10} volume:{:.5f}".format(formatted_timestamp,
+                                                                                formatted_price, volume)
+                        # print(formatted_line)
+                    else:
+                        print("Invalid data entry:", entry)
+            else:
+                print("Invalid JSON data format.")
+        except (json.JSONDecodeError, KeyError) as e:
+            print("Error processing JSON data:", e)
 
-    except json.JSONDecodeError:
-        print("Error decoding JSON data.")
+    def calculate_averages(self, current_time):
+        if len(self.raw_data) > 0:
+            minute_data = {}
+
+            for entry in self.raw_data:
+                timestamp = datetime.datetime.fromtimestamp(entry['timestamp'])
+                minute_key = timestamp.strftime('%Y-%m-%d %H:%M')
+                if minute_key not in minute_data:
+                    minute_data[minute_key] = {'total_price_volume': 0, 'total_volume': 0}
+                minute_data[minute_key]['total_price_volume'] += entry['price'] * entry['volume']
+                minute_data[minute_key]['total_volume'] += entry['volume']
+
+            self.raw_data = []  # Reset raw data after accumulating data
+
+            # Calculate and print VWAP for each unique interval
+            for minute_key, data in minute_data.items():
+                if data['total_volume'] != 0:
+                    vwap = data['total_price_volume'] / data['total_volume']
+                    start_time = datetime.datetime.strptime(minute_key, '%Y-%m-%d %H:%M')
+                    end_time = start_time + datetime.timedelta(minutes=1)
+                    formatted_interval = f"{start_time.strftime('%Y-%m-%d %H:%M')}-{end_time.strftime('%H:%M')}"
+
+                    # Calculate the time to sleep based on the last printed interval
+                    if self.last_average_time is None:
+                        sleep_time = 2 * 60  # Sleep 2 minutes for the first interval
+                    else:
+                        sleep_time = 60
+
+                    time.sleep(sleep_time)
+                    self.last_average_time = end_time  # Update the last average time
+
+                    print(f"Interval: {formatted_interval} - Volume-Weighted Average Price: {vwap:.2f}")
+
+    def on_message(self, ws, message):
+        self.process_live_data(message)
+        current_time = datetime.datetime.now() - datetime.timedelta(hours=3)  # Adjusted time
+        self.calculate_averages(current_time)
+
+    def on_error(self, ws, error):
+        print(error)
+
+    def on_close(self, ws, close_status_code, close_msg):
+        print("### closed ###")
+
+    def on_open(self, ws):
+        ws.send('{"type":"subscribe","symbol":"AAPL"}')
+        ws.send('{"type":"subscribe","symbol":"AMZN"}')
+        ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
+        ws.send('{"type":"subscribe","symbol":"IC MARKETS:1"}')
 
 
-def on_message(ws, message):
-    process_live_data(message)
-
-
-def on_error(ws, error):
-    print(error)
-
-
-def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
-
-
-def on_open(ws):
-    ws.send('{"type":"subscribe","symbol":"AAPL"}')
-    ws.send('{"type":"subscribe","symbol":"AMZN"}')
-    ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
-    ws.send('{"type":"subscribe","symbol":"IC MARKETS:1"}')
